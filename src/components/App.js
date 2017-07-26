a30fd75f688dfe5d2a17c88a1a69bb37b3492be1
Ian Thompson <thompsonian88@gmail.com>
2017-07-26 17:08:09 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import BaseLayout from './BaseLayout';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <BaseLayout />
+      </div>
+    );
+  }
+}
+
+
+export default App;
+
